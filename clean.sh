2160d5d37ffc0593f16ab58719292609d27be579
AWS ParallelCluster user <ubuntu@ip-10-0-0-239.us-west-2.compute.internal>
2024-07-26 00:30:45 +0000

    Added slurmBenchmarker

--- clean.sh
+++ clean.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -e
-rm -rf slurm*.out log.lammp log.lammps lammps.{o,e}* lammps*.out lammps*.err lammps*.sbatch
-
+rm -rf slurm*.out log.lammp log.lammps lammps.{o,e}* */*/lammps*.out */*/lammps*.err */*/lammps*.sbatch
